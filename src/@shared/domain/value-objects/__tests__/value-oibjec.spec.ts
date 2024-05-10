920ef655535988f44daf972a7cd3b3795b34fcbc
pedro-valbusa <pedro.valbusa@meta.com.br>
2024-05-10 11:40:22 +0000

    test:m add more teste to value object

--- src/@shared/domain/value-objects/__tests__/value-oibjec.spec.ts
+++ src/@shared/domain/value-objects/__tests__/value-oibjec.spec.ts
@@ -1,7 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ValueObject } from '../value-object'
 
 class StringValueObject extends ValueObject {
+  constructor(readonly value: string) {
+    super()
+  }
+}
+
+class SecondStringValueObject extends ValueObject {
   constructor(readonly value: string) {
     super()
   }
@@ -16,8 +21,8 @@ class ComplexValueObject extends ValueObject {
   }
 }
 
-describe('ValueObject Unit Tests', () => {
-  test('should be equals', () => {
+describe('ValueObject Unit Test', () => {
+  it('should be equals', () => {
     const valueObject1 = new StringValueObject('test')
     const valueObject2 = new StringValueObject('test')
     expect(valueObject1.equals(valueObject2)).toBeTruthy()
@@ -27,17 +32,20 @@ describe('ValueObject Unit Tests', () => {
     expect(complexValueObject1.equals(complexValueObject2)).toBeTruthy()
   })
 
-  test('should not be equals', () => {
+  it('should not be equals', () => {
     const valueObject1 = new StringValueObject('test')
     const valueObject2 = new StringValueObject('test2')
     expect(valueObject1.equals(valueObject2)).toBeFalsy()
     expect(valueObject1.equals(null as any)).toBeFalsy()
-    expect(valueObject1.equals(null as any)).toBeFalsy()
+    expect(valueObject1.equals(undefined as any)).toBeFalsy()
 
     const complexValueObject1 = new ComplexValueObject('test', 1)
     const complexValueObject2 = new ComplexValueObject('test', 2)
     expect(complexValueObject1.equals(complexValueObject2)).toBeFalsy()
     expect(complexValueObject1.equals(null as any)).toBeFalsy()
-    expect(complexValueObject1.equals(null as any)).toBeFalsy()
+    expect(complexValueObject1.equals(undefined as any)).toBeFalsy()
+
+    const secondStringValueObject = new SecondStringValueObject('test')
+    expect(valueObject1.equals(secondStringValueObject)).toBeFalsy()
   })
 })
