0dba4c9ba90375a187b4cf4788de2b0e2d700404
pedro-valbusa <pedro.valbusa@meta.com.br>
2024-05-10 11:47:39 +0000

    feat: create exception error

---
+++ src/@shared/errors/exceptions/exceptions-error.ts
@@ -0,0 +1,32 @@
+import { ErrorObject } from '../dictionary/dictionary-types'
+
+export class ExceptionError extends Error {
+  public readonly errorObject: ErrorObject
+
+  constructor(
+    code: string,
+    title: string,
+    detail: string,
+    status: string,
+    statusCode: number,
+    meta?: Record<string, unknown>,
+    action?: string,
+    source?: Record<string, unknown>,
+    children?: Record<string, unknown>[],
+    resolution?: string,
+  ) {
+    super(title)
+    this.errorObject = {
+      status,
+      statusCode,
+      code,
+      title,
+      detail,
+      source,
+      meta,
+      action,
+      children,
+      resolution,
+    }
+  }
+}
